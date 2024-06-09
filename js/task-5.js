2ab94a0f6a9760a6744f09f64341c658d4903cbf
Nadiia-Yuce <159655565+Nadiia-Yuce@users.noreply.github.com>
2024-06-09 14:05:41 +0200

    done

--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,15 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const button = document.querySelector(".change-color");
+const body = document.querySelector("body");
+const span = document.querySelector(".color");
+
+button.addEventListener("click", handlerClick);
+
+function handlerClick() {
+  const randomColor = getRandomHexColor();
+  body.style.backgroundColor = randomColor;
+  span.textContent = randomColor;
+}
--- js/task-6.js
+++ js/task-6.js
@@ -3,3 +3,39 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const createButton = document.querySelector("button[data-create]");
+const destroyButton = document.querySelector("button[data-destroy]");
+const boxes = document.getElementById("boxes");
+const input = document.querySelector("input");
+
+createButton.addEventListener("click", handlerCreate);
+destroyButton.addEventListener("click", destroyBoxes);
+
+function createBoxes(amount) {
+  boxes.innerHTML = "";
+
+  for (let i = 0; i < amount; i++) {
+    const box = document.createElement("div");
+    const boxSize = 30 + i * 10;
+    box.classList.add("boxStyle");
+    box.style.width = `${boxSize}px`;
+    box.style.height = `${boxSize}px`;
+    box.style.backgroundColor = getRandomHexColor();
+    boxes.appendChild(box);
+  }
+  input.value = "";
+}
+
+function destroyBoxes() {
+  boxes.innerHTML = "";
+}
+
+function handlerCreate() {
+  const amount = parseInt(input.value);
+  if (!isNaN(amount) && amount >= 1 && amount <= 100) {
+    createBoxes(amount);
+  } else {
+    alert("A number should be between 1 and 100");
+  }
+}
